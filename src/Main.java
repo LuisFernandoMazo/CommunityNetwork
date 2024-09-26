0f1f8a7cf75f4e554fce6eb8691094c0c86e4afe
tomasMunoz <tomuru31@gmail.com>
2024-09-25 21:38:17 -0500

    adición de clases para ciudad y departamento

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Ciudad> ciudadesAntioquia =new ArrayList<>();
+        ciudadesAntioquia.add(new Ciudad(1, "Medellín"));
+        ciudadesAntioquia.add(new Ciudad(2, "Envigado"));
+        ciudadesAntioquia.add(new Ciudad(3, "Itagüí"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Departamento departamento = new Departamento(1, "Antioquia", ciudadesAntioquia);
+
+        for (Ciudad ciudad: ciudadesAntioquia){
+            ciudad.setDepartamento(departamento);
+        }
+
+        if (departamento.estaRegistrado()) {
+            System.out.println("El departamento está registrado.");
+            departamento.mostrarInformacion();
+        } else {
+            System.out.println("El departamento no está registrado.");
+        }
+
+        Ciudad ciudad = new Ciudad(1, "Medellín");
+
+        if (ciudad.estaRegistrada()) {
+            System.out.println("La ciudad está registrada.");
+            ciudad.mostrarInformacion();
+        } else {
+            System.out.println("La ciudad no está registrada.");
         }
     }
 }
